2460527bec3d9e28b877e0164b54f886c029d676
unknown <manip@diamant.lpa.ens.fr>
2020-10-16 19:04:17 +0200

    tata

---
+++ data/20201014/traitement.py
@@ -0,0 +1,210 @@
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+from PyQt5.QtWidgets import QApplication,QFileDialog
+
+
+
+
+def extract_data(filename,xcol=0,ycol=1):
+	x=[]
+	y=[]
+	with open(filename,'r',encoding = "ISO-8859-1") as f:
+		for line in f :
+			line=line.split()
+			try :
+				x+=[float(line[xcol])]
+				y+=[float(line[ycol])]
+			except :
+				pass
+	return(np.array(x),np.array(y))
+
+def lin_fit(x,y) :
+	A=np.vstack([x,np.ones(len(x))]).T
+	a,b = np.linalg.lstsq(A, y, rcond=None)[0]
+	return(a,b,a*x+b)
+
+def quad_fit(x,y) :
+	A=np.vstack([x**2,x,np.ones(len(x))]).T
+	a,b,c = np.linalg.lstsq(A, y, rcond=None)[0]
+	return([a,b,c],a*x**2+b*x+c)
+
+def fit_ordre_4(x,y) :
+	A=np.vstack([x**4,x**3,x**2,x,np.ones(len(x))]).T
+	a,b,c,d,e = np.linalg.lstsq(A, y, rcond=None)[0]
+	return([a,b,c,d,e],a*x**4+b*x**3+c*x**2+d*x+e)
+
+def gauss_fit(x,y,Amp=None,x0=None,sigma=None,ss=0) :
+	if not ss :
+		ss=y[0]
+	if not Amp :
+		if max(y)-ss > ss-min(y) :
+			Amp=max(y)-ss
+		else :
+			Amp=min(y)-ss
+	if not x0 :
+		x0=x[int(len(x)/2)]
+	if not sigma :
+		sigma=x[int(len(x)/5)]-x[0]
+	def f(x,Amp,x0,sigma,ss) :
+		return Amp*np.exp(-((x-x0)/(2*sigma))**2)+ss
+	p0=[Amp,x0,sigma,ss]
+	popt, pcov = curve_fit(f, x, y, p0)
+	return(popt,f(x,popt[0],popt[1],popt[2],popt[3]))
+
+def lor_fit(x,y,Amp=None,x0=None,sigma=None,ss=None) :
+	if not ss :
+		ss=y[0]
+	if not Amp :
+		if max(y)-ss > ss-min(y) :
+			Amp=max(y)-ss
+		else :
+			Amp=min(y)-ss
+	if not x0 :
+		x0=x[int(len(x)/2)]
+	if not sigma :
+		sigma=x[int(len(x)/5)]-x[0]
+	def f(x,Amp,x0,sigma,ss) :
+		return ss+Amp*1/(1+((x-x0)/(2*sigma))**2)
+	p0=[Amp,x0,sigma,ss]
+	popt, pcov = curve_fit(f, x, y, p0)
+	return(popt,f(x,popt[0],popt[1],popt[2],popt[3]))
+
+def exp_fit(x,y,Amp=None,ss=None,tau=None) :
+	if not Amp :
+		Amp=max(y)-min(y)
+	if not ss :
+		ss=y[-1]
+	if not tau :
+		tau=x[int(len(x)/10)]-x[0]
+	def f(x,Amp,ss,tau) :
+		return Amp*np.exp(-x/tau)+ss
+	p0=[Amp,ss,tau]
+	popt, pcov = curve_fit(f, x, y, p0)
+	return(popt,f(x,popt[0],popt[1],popt[2]))
+
+def stretch_exp_fit(x,y,Amp=None,ss=None,tau=None) :
+	if not Amp :
+		Amp=max(y)-min(y)
+	if not ss :
+		ss=y[-1]
+	if not tau :
+		tau=x[int(len(x)/10)]-x[0]
+	def f(x,Amp,ss,tau) :
+		return Amp*np.exp(-np.sqrt(x/tau))+ss
+	p0=[Amp,ss,tau]
+	popt, pcov = curve_fit(f, x, y, p0)
+	return(popt,f(x,popt[0],popt[1],popt[2]))
+
+def stretch_soustraction(x,y,Amp=None,tau=None) :
+	if not Amp :
+		Amp=max(y)-min(y)
+	if not tau :
+		tau=x[int(len(x)/10)]-x[0]
+	def f(x,Amp,tau) :
+		return Amp*np.exp(-np.sqrt(x/tau))
+	p0=[Amp,tau]
+	popt, pcov = curve_fit(f, x, y, p0)
+	return(popt,f(x,popt[0],popt[1]))
+
+def third_stretch(x,y,Amp=None,tau=None) :
+	if not Amp :
+		Amp=max(y)-min(y)
+	if not tau :
+		tau=x[int(len(x)/10)]-x[0]
+	def f(x,Amp,tau) :
+		return Amp*np.exp(-(x/tau)^(1/3))
+	p0=[Amp,tau]
+	popt, pcov = curve_fit(f, x, y, p0)
+	return(popt,f(x,popt[0],popt[1]))
+
+def stretch_et_phonon(x,y,Amp=None,tau=None) :
+	if not Amp :
+		Amp=max(y)-min(y)
+	if not tau :
+		tau=x[int(len(x)/10)]-x[0]
+	def f(x,Amp,tau) :
+		return Amp*np.exp(-np.sqrt(x/tau)-x/5E-3)
+	p0=[Amp,tau]
+	popt, pcov = curve_fit(f, x, y, p0)
+	return(popt,f(x,popt[0],popt[1]))
+
+def fit_B_dipole(x,y,B0=2000,x0=10) :
+	def f(x,B0,x0):
+		return(B0/(x+x0)**3)
+	p0=[B0,x0]
+	popt, pcov = curve_fit(f, x, y, p0)
+	return(popt,f(x,popt[0],popt[1]))
+
+def find_B_111(freq,transi='-') : #freq en MHz
+	D=2870
+	gamma=2.8
+	if transi=='-' :
+		return(D-freq)/gamma
+	elif transi =='+' :
+		return(freq-D)/gamma
+
+
+
+def extract_2d(fname):
+	data=[]
+	with open(fname,'r',encoding = "ISO-8859-1") as f:
+		for line in f:
+			line=line.split()
+			try :
+				row=[float(elem) for elem in line]
+				data+=[row]
+			except :
+				pass
+	return(np.array(data))
+
+def print_map(array,xmin=0,xmax=1,ymin=0,ymax=1):
+	x_axis=np.linspace(xmin,xmax,len(array[0,:]))
+	y_axis=np.linspace(ymin,ymax,len(array[:,0]))
+	fig,ax=plt.subplots()
+	c=ax.pcolormesh(x_axis, y_axis, array, cmap='plasma')
+	cb=fig.colorbar(c,ax=ax)
+	plt.show()
+
+
+
+def ask_name():
+	qapp = QApplication(sys.argv)
+	fname,filters=QFileDialog.getOpenFileName()	
+	return fname
+
+
+
+xy=extract_2d('scan_ESR.txt')
+
+
+for i in range(len(xy[:,0])):
+	xy[i,:]=xy[i,:]/max(xy[i,:])
+
+xy[8,:]=np.roll(xy[8,:],-420)
+# print_map(xy,xmin=500,xmax=3000,ymin=-15,ymax=0)
+
+pos_min=[]
+for line in xy:
+	pos_min+=[line.argmin()]
+#pos_min=pos_min[:19]+pos_min[23:33]
+pos_min=np.array(pos_min)
+pos_min=500+pos_min*2.5/2
+x_pos=np.linspace(15,0,len(pos_min))
+
+
+
+pos_min=find_B_111(pos_min)
+pos_min[35:]=2040-pos_min[35:]
+nmax=19
+popt,yfit=fit_B_dipole(x_pos[:nmax],pos_min[:nmax])
+B0,x0=popt[0],popt[1]
+
+x_axis=np.linspace(500,3000,len(xy[0,:]))
+y_axis=(B0/(x_pos+x0)**3)
+fig,ax=plt.subplots()
+c=ax.pcolormesh(x_axis, y_axis, xy, cmap='plasma')
+cb=fig.colorbar(c,ax=ax)
+plt.show()
+plt.show()
