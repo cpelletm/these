6931c705a8238770deb9f55d625b77a4b7eb6df9
Clement Pellet-Mary <clement.pelletmary@gmail.com>
2020-11-01 11:50:12 +0100

    demenagement

--- python/test.py
+++ python/test.py
@@ -1,19 +1,6 @@
 import numpy as np 
 import matplotlib.pyplot as plt
-from numpy.random import random as rd
-from datetime import date
 
-x=np.linspace(0,2,200)
-y=np.arctan(np.tan(np.pi/2*x))
 
-plt.plot(x,y,label='arctan(tan(pi/2*x))')
-ax=plt.gca()
-ylim=ax.get_ylim()
-xlim=ax.get_xlim()
-plt.plot([-1,3],[0,0],linewidth=3,color='r')
-plt.plot([3/2,3/2],[-2,2],ls='--',color='r')
-ax.set_xlim(xlim)
-ax.set_ylim(ylim)
-plt.grid()
-plt.legend()
-plt.show()
+print('%E'%(1E-28*5E-6/(2/5*12.011*1.66E-27*(7.5E-6)**2*1E6)))
+print('%E'%(12.011*1.66E-27))
