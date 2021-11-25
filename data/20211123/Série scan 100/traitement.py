ede2db2ca94388a4b7c3819fed6b8bebdd3629c3
cpelletm <68075485+cpelletm@users.noreply.github.com>
2021-11-25 14:59:40 +0100

    tutu

--- data/20211123/Série scan 100/traitement.py
+++ data/20211123/Série scan 100/traitement.py
@@ -8,22 +8,24 @@ from analyse import *
 n=30
 xs=np.linspace(0,10,n)
 
-data=np.zeros((n,n))
-for j in range(n) :
-	print(j)
-	for i in range(n) :
-		x,y=extract_data('x=%.6f,y=%.6f'%(xs[i],xs[j]))
-		x2,y2=extract_data('x=%.6f,y=%.6f'%(xs[i],xs[j]),xcol=2,ycol=3)
-		data[i,j]=(sum(y[256:264])-sum(y[264:271]))/sum(y2)
-data=data.T
-print_map(data[0:27,0:])
+# data=np.zeros((n,n))
+# for j in range(n) :
+# 	print(j)
+# 	for i in range(n) :
+# 		x,y=extract_data('x=%.6f,y=%.6f'%(xs[i],xs[j]))
+# 		x2,y2=extract_data('x=%.6f,y=%.6f'%(xs[i],xs[j]),xcol=2,ycol=3)
+# 		# data[i,j]=(sum(y[256:264])-sum(y[264:271]))/sum(y2)
+# 		data[i,j]=(max(y[10:70])-min(y[10:70]))/(max(y[280:295])-min(y[225:255]))
+# data=data.T
+# print_map(data[0:,0:])
 
 
 
-# x,y=extract_data('x=%.6f,y=%.6f'%(xs[1],xs[1]))
-# plt.plot(y,'x-')
+x,y=extract_data('x=%.6f,y=%.6f'%(xs[1],xs[29]))
+plt.plot(y,'x-')
 
-# # y[256:264]-y[264:271]
-
-
-# plt.show()
+# y[256:264]-y[264:271]
+max(y[10:70])-min(y[10:70])
+max(y[280:295])-min(y[225:255])
+max(y[254:273])-min(y[254:273])
+plt.show()
--- python_clément/scan EM.py
+++ python_clément/scan EM.py
@@ -4,22 +4,28 @@ from lab import *
 nSide=30
 xs=np.linspace(0,10,nSide)
 ys=np.linspace(0,10,nSide)
+laser_freqs=np.linspace(2E6,2E7,10)
+
 
 def acquiStart(i):
-	ix=i%nSide
-	iy=i//nSide
-	xV=xs[ix]
-	yV=ys[iy]
-	cube.move(xV,ax='x')	
-	cube.move(yV,ax='y')
+	# ix=i%nSide
+	# iy=i//nSide
+	# xV=xs[ix]
+	# yV=ys[iy]
+	# cube.move(xV,ax='x')	
+	# cube.move(yV,ax='y')
+	laser.laserFreq.setValue(laser_freqs[i])
+	laser.lasOnOff()
 
 
 def acquiEnd(i):
-	ix=i%nSide
-	iy=i//nSide
-	xV=xs[ix]
-	yV=ys[iy]
-	fname=StartStop.defaultFolder+'x=%f,y=%f'%(xV,yV)
+	# ix=i%nSide
+	# iy=i//nSide
+	# xV=xs[ix]
+	# yV=ys[iy]
+	# fname=StartStop.defaultFolder+'x=%f,y=%f'%(xV,yV)
+	laser.laser.stop()
+	fname='fLas=%f'%laser_freqs[i]
 	if i==0 :
 		save.save(fname=fname,saveFigure=True)
 	else :
@@ -60,13 +66,13 @@ cube=PiezoCube3axes()
 
 ## Setup the Graphical interface ##
 
-
+laser=pulsedLaserWidget()
 vmin=field('V min (V)',-5,spaceAbove=3)
 vmax=field('V max (V)',5,spaceAbove=0)
 nPoints=field('n points',501,spaceAbove=3)
 fsweep=field('sweep frequency (Hz)',200,spaceAbove=0)
 
-fields=[vmin,vmax,nPoints,fsweep]
+fields=[laser,vmin,vmax,nPoints,fsweep]
 
 gra=graphics()
 l1=gra.addLine(typ='average',style='lm')
@@ -74,7 +80,7 @@ ax2=gra.addAx()
 l2=gra.addLine(typ='average',style='lm',ax=ax2)
 
 StartStop=startStopButton(setup=setup,update=update,debug=True,serie=True,lineIter=l1,extraStop=lambda: ao.setTo(0))
-StartStop.setupSerie(nAcqui=nSide**2,iterPerAcqui=10,acquiStart=acquiStart,acquiEnd=acquiEnd)
+StartStop.setupSerie(nAcqui=10,iterPerAcqui=100,acquiStart=acquiStart,acquiEnd=acquiEnd)
 save=saveButton(gra,autoSave=False)
 trace=keepTraceButton(l1,l2)
 it=iterationWidget(l1)
