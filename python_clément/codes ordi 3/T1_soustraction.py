01f78b6346fba1fa4fd389bb8ac3660e3f02730f
cpelletm <68075485+cpelletm@users.noreply.github.com>
2022-01-20 16:38:55 +0100

    Update T1_soustraction.py

--- python_clément/codes ordi 3/T1_soustraction.py
+++ python_clément/codes ordi 3/T1_soustraction.py
@@ -18,9 +18,13 @@ def setup():
 	#ch1=laser, ch2= ??, ch3= mw, ch4=PL
 	pb.addLine(ch1=1,dt=tpola+tread,unit='us')
 	for t in tlist :
+		if moins1.state() :
+			pb.addLine(ch1=0,ch3=1,dt=tpulse,unit='ns')
 		pb.addLine(ch1=0,dt=t,unit='ms')
 		pb.addPulses(ch1=1,ch4=2,dt=dtAcqui,unit='s',nLoop=nAvg)
 		pb.addLine(ch1=1,dt=tpola,unit='us')
+		if moins1.state() :
+			pb.addLine(ch1=0,ch3=1,dt=tpulse,unit='ns')
 		pb.addLine(ch1=0,dt=t,unit='ms')
 		pb.addLine(ch1=0,ch3=1,dt=tpulse,unit='ns')
 		pb.addPulses(ch1=1,ch4=2,dt=dtAcqui,unit='s',nLoop=nAvg)
@@ -39,9 +43,13 @@ def setup():
 def update(x):
 	if ai.done() :
 		PL=ai.read()
-		y=[PL[2*k]-PL[2*k+1] for k in range(val(nPoints))]
+		y1=[PL[2*k]for k in range(val(nPoints))]
+		y2=[PL[2*k+1]for k in range(val(nPoints))]
+		y3=y1-y2
 		pb.start()
-		gra.updateLine(l1,x,y) 
+		gra.updateLine(l1,x,y1) 
+		gra.updateLine(l2,x,y2) 
+		gra.updateLine(l3,x,y3) 
 
 
 ## Create the communication (I/O) instances ##
@@ -53,16 +61,20 @@ ai=AIChan()
 uwFreq=field('mw freq (MHz)',2791)
 uwPower=field('mw power (dBm)',15,spaceAbove=0)
 tpulse=field('t pulse pi (ns)',510,spaceAbove=0)
+moins1=checkBox('Pulse at the beginning',spaceAbove=0)
 
 tscan=field('t scan (ms)',15)
 nPoints=field('n points pulse',151,spaceAbove=0)
 
 tpola=field('t polarisation (µs)',500)
 tread=field('t read (µs)',500,spaceAbove=0)
-fields=[uwFreq,uwPower,tpulse,tscan,nPoints,tpola,tread]
+fields=[uwFreq,uwPower,tpulse,moins1,tscan,nPoints,tpola,tread]
 
 gra=graphics(theme='black')
 l1=gra.addLine(typ='average',style='lm',fast=True)
+l2=gra.addLine(typ='average',style='lm',fast=True)
+ax2=gra.addAx()
+l3=gra.addLine(ax=ax2,typ='average',style='lm',fast=True)
 #typ='scroll' for scrolling data, 'average' for averaging data or 'instant' for immediate data
 #style='l', 'm' or 'lm' for lines and/or marker
 #fast=True/False (default False) : apply antialias or not on the lines. With anti-alias (fast=False), it takes ~0.1s to update a set with a lot of lines
@@ -70,9 +82,9 @@ l1=gra.addLine(typ='average',style='lm',fast=True)
 channels=dropDownMenu('Channel to read :',*physicalChannels,spaceAbove=0)
 StartStop=startStopButton(setup=setup,update=update,debug=True)
 save=saveButton(gra,autoSave=False)
-trace=keepTraceButton(l1)
-expfit=fitButton(line=l1,fit='expZero',name='exp fit')
-stretchfit=fitButton(line=l1,fit='stretchZero',name='stretch fit')
+trace=keepTraceButton(l1,l2,l3)
+expfit=fitButton(line=l3,fit='expZero',name='exp fit')
+stretchfit=fitButton(line=l3,fit='stretchZero',name='stretch fit')
 it=iterationWidget(l1)
 norm=gra.normalize()
 norm.setState(False)
